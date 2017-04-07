9cf26b3db8d92d8159b56e7c29a52b4131ecfcfe
AlecLucas <aleclucas28@Gmail.com>
2017-04-07 12:53:49 +0800

    Add files via upload
    Added the functions for the rest of the conversions and made the second button work.

---
+++ ConverterApp/ConverterApp/ConverterApp/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace ConverterApp
+{
+    partial class frm_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lbDisplay = new System.Windows.Forms.ListBox();
+            this.txtCmToInch = new System.Windows.Forms.TextBox();
+            this.txtMetreToFeet = new System.Windows.Forms.TextBox();
+            this.txtCelsiusToFahrenheit = new System.Windows.Forms.TextBox();
+            this.txtCmToFeet = new System.Windows.Forms.TextBox();
+            this.txtKiloToMiles = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(16, 246);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(230, 9);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lbDisplay
+            // 
+            this.lbDisplay.FormattingEnabled = true;
+            this.lbDisplay.Location = new System.Drawing.Point(160, 57);
+            this.lbDisplay.Name = "lbDisplay";
+            this.lbDisplay.Size = new System.Drawing.Size(145, 212);
+            this.lbDisplay.TabIndex = 2;
+            // 
+            // txtCmToInch
+            // 
+            this.txtCmToInch.Location = new System.Drawing.Point(16, 57);
+            this.txtCmToInch.Name = "txtCmToInch";
+            this.txtCmToInch.Size = new System.Drawing.Size(100, 20);
+            this.txtCmToInch.TabIndex = 3;
+            // 
+            // txtMetreToFeet
+            // 
+            this.txtMetreToFeet.Location = new System.Drawing.Point(16, 96);
+            this.txtMetreToFeet.Name = "txtMetreToFeet";
+            this.txtMetreToFeet.Size = new System.Drawing.Size(100, 20);
+            this.txtMetreToFeet.TabIndex = 4;
+            // 
+            // txtCelsiusToFahrenheit
+            // 
+            this.txtCelsiusToFahrenheit.Location = new System.Drawing.Point(16, 135);
+            this.txtCelsiusToFahrenheit.Name = "txtCelsiusToFahrenheit";
+            this.txtCelsiusToFahrenheit.Size = new System.Drawing.Size(100, 20);
+            this.txtCelsiusToFahrenheit.TabIndex = 5;
+            // 
+            // txtCmToFeet
+            // 
+            this.txtCmToFeet.Location = new System.Drawing.Point(16, 175);
+            this.txtCmToFeet.Name = "txtCmToFeet";
+            this.txtCmToFeet.Size = new System.Drawing.Size(100, 20);
+            this.txtCmToFeet.TabIndex = 6;
+            // 
+            // txtKiloToMiles
+            // 
+            this.txtKiloToMiles.Location = new System.Drawing.Point(16, 214);
+            this.txtKiloToMiles.Name = "txtKiloToMiles";
+            this.txtKiloToMiles.Size = new System.Drawing.Size(100, 20);
+            this.txtKiloToMiles.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Units of conversion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Centimetres to Inches.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Metres to Feet";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Celsius to Fahrenheit";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Centimetres to Feet";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 198);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Kilometres to Miles";
+            // 
+            // frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 284);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKiloToMiles);
+            this.Controls.Add(this.txtCmToFeet);
+            this.Controls.Add(this.txtCelsiusToFahrenheit);
+            this.Controls.Add(this.txtMetreToFeet);
+            this.Controls.Add(this.txtCmToInch);
+            this.Controls.Add(this.lbDisplay);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnConvert);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "frm_Main";
+            this.Text = "ATCA Gas Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListBox lbDisplay;
+        private System.Windows.Forms.TextBox txtCmToInch;
+        private System.Windows.Forms.TextBox txtMetreToFeet;
+        private System.Windows.Forms.TextBox txtCelsiusToFahrenheit;
+        private System.Windows.Forms.TextBox txtCmToFeet;
+        private System.Windows.Forms.TextBox txtKiloToMiles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ ConverterApp/ConverterApp/ConverterApp/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConverterApp
+
+{
+    public partial class frm_Main : Form
+    {
+        public frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        double[] Conversions = new double[5];
+
+        double unitConvert;
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void btnConvert_Click(object sender, EventArgs e)
+        {
+            //Clearing listbox before use
+            lbDisplay.Items.Clear();
+
+            //Centimetres to Inches outputs
+            lbDisplay.Items.Add("Centimetres to Inches:");
+            lbDisplay.Items.Add(CmToInches());
+            lbDisplay.Items.Add("");
+
+            // Metres to Feet outputs
+            lbDisplay.Items.Add("Metres to Feet:");
+            lbDisplay.Items.Add(MetresToFeet());
+            lbDisplay.Items.Add("");
+
+            //Celsius to Fehrenheit outputs
+            lbDisplay.Items.Add("Celsius to Fehrenheit:");
+            lbDisplay.Items.Add(CelsiusToFahrenheit() + " °F");
+            lbDisplay.Items.Add("");
+
+            //Centermetres to Feet outputs
+            lbDisplay.Items.Add("Centermetres to Feet:");
+            lbDisplay.Items.Add(CmToFeet());
+            lbDisplay.Items.Add("");
+
+            //Kilometres to Miles outputs
+            lbDisplay.Items.Add("Kilometres to Miles:");
+            lbDisplay.Items.Add(KilometresToMiles());
+            lbDisplay.Items.Add("");
+
+        }
+
+        public double CmToInches()
+        {
+            const double CmToInchConversion = 0.3937;
+
+            if (! double.TryParse(txtCmToInch.Text, out Conversions[0]))
+            {
+                MessageBox.Show("Invalid unit in Centermetres to Inches textbox.");
+                txtCmToInch.Clear();
+                txtCmToInch.Focus();            
+            }
+            else
+            {
+                unitConvert = Conversions[0] * CmToInchConversion;
+            }
+
+            return unitConvert;
+        }
+
+        public double MetresToFeet()
+        {
+            const double MetresToFeetConversion = 3.28084;
+
+            if (!double.TryParse(txtMetreToFeet.Text, out Conversions[1]))
+            {
+                MessageBox.Show("Invalid unit in Metres to Feet textbox.");
+                txtMetreToFeet.Clear();
+                txtMetreToFeet.Focus();
+            }
+            else
+            {
+                unitConvert = Conversions[1] * MetresToFeetConversion;
+            }
+
+            return unitConvert;
+        }
+
+        public double CelsiusToFahrenheit()
+        {
+            const double CelsiusToFahrenheitConversion = 30.48;
+
+            if (!double.TryParse(txtCelsiusToFahrenheit.Text, out Conversions[2]))
+            {
+                MessageBox.Show("Invalid unit in Celsius to Fahrenheit textbox.");
+                txtCelsiusToFahrenheit.Clear();
+                txtCelsiusToFahrenheit.Focus();
+            }
+            else
+            {
+                unitConvert = Conversions[2] / CelsiusToFahrenheitConversion;
+            }
+
+            return unitConvert;
+        }
+
+        public double CmToFeet()
+        {
+            const double CmToFeetConversion = 0.0328084;
+
+            if (!double.TryParse(txtCmToFeet.Text, out Conversions[3]))
+            {
+                MessageBox.Show("Invalid unit in Centermetres to Feet textbox.");
+                txtCmToFeet.Clear();
+                txtCmToFeet.Focus();
+            }
+            else
+            {
+                unitConvert = Conversions[3] * CmToFeetConversion;
+            }
+
+            return unitConvert;
+        }
+
+        public double KilometresToMiles()
+        {
+            const double KilometresToMilesConversion = 0.621371;
+
+            if (!double.TryParse(txtKiloToMiles.Text, out Conversions[4]))
+            {
+                MessageBox.Show("Invalid unit in Kilometres to Miles textbox.");
+                txtKiloToMiles.Clear();
+                txtKiloToMiles.Focus();
+            }
+            else
+            {
+                unitConvert = Conversions[4] * KilometresToMilesConversion;
+            }
+
+            return unitConvert;
+        }
+    }
+}
